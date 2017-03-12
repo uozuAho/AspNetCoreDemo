d7ac2f2f082754482dceaa80e8a30a8f8b141cc4
Warwick Stone <uozu.aho@gmail.com>
2017-03-12 23:07:14 +1100

    add models as per ef/postgres example

---
+++ Models/ArticleContext.cs
@@ -0,0 +1,11 @@
+using Microsoft.EntityFrameworkCore;
+
+public class ArticleContext : DbContext  
+{
+    public ArticleContext(DbContextOptions<ArticleContext> options)
+        : base(options)
+    { }
+
+    public DbSet<Article> Articles { get; set; }
+    public DbSet<Author> Authors { get; set; }
+}
